09ed83de0e5f356f8888d5372f7be127581d897b
dsszymanski <dan1993us@yahoo.com>
2022-08-23 23:48:52 -0400

    Documented Textbox.

--- application-tracker/src/components/TextBox.js
+++ application-tracker/src/components/TextBox.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import './TextBox.css'
+import '../component-css/TextBox.css'
 
+/**
+ * Basic text box used to display one line application data. Takes
+ * in a label prop and a text prop to display in the application
+ * component. Will return null if no label or text is supplied in props.
+ *
+ * @param {string} props.label: input string used to describe the text.
+ * @param {string} props.text: input string of data to display on screen.
+ *
+ * @returns component.
+ */
 const TextBox = ( props ) => {
+  //Returns null if label or text is not in props.
   return (
     (props.label && props.text) ?
     <div className='text-box-container'>
