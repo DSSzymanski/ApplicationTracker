8484da7a2b2ab0c2afe5c9d9e8b00465d9d4e13f
dsszymanski <dan1993us@yahoo.com>
2022-09-01 21:35:33 -0400

    Adjusted the data fields. Added button that switches the components displays for editting fields.

--- application-tracker/src/components/Application.js
+++ application-tracker/src/components/Application.js
@@ -1,35 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import TextBox from './TextBox';
+import TextBoxLink from './TextBoxLink';
 
 const Application = (props) => {
+    const EDIT_TEXT = "Edit";
+    const SAVE_TEXT = "Save";
+
     const getInitialState = (props) => {
         return {
-            'job-title': props.count,
-            'job-notes': "",
-            'company': "",
-            'company-notes': "",
-            'cv-used': "",
-            'application-used': "",
-            'application-status': "",
-            'application-notes': "",
-            'progress': "",
-            'progress-notes': "",
+            'job-title': '' + props.count,
+            'job-link': 'www.google.com',
+            'job-notes': "line1 " + props.count + '\n' + 'line2.',
+            'company': 'company',
+            'company-notes': "company-notes",
+            'application-status': "application-status",
+            'application-notes': "application-notes",
         }
     }
     let [ applicationData, setApplicationData ] = useState(getInitialState(props));
+    let [ buttonText, setButtonText ] = useState(EDIT_TEXT);
+
+    let updateButtonText = () => {
+        buttonText === EDIT_TEXT ?
+        setButtonText(SAVE_TEXT) :
+        setButtonText(EDIT_TEXT);
+    }
 
     return (
         <div className="application-div">
-            {Object.entries(applicationData).map((data, index) => {
-                return (
-                    <TextBox
-                        key={ index }
-                        label={ data[0] }
-                        text={ data[1] }
-                    />
-                );
-            })}
+            <div className="application-data-div">
+                {
+                    buttonText === SAVE_TEXT ?
+                    <EditApplication data={ applicationData } /> :
+                    <DisplayApplication data={ applicationData } />
+                }
+            </div>
+            <div className="application-button-container">
+                <button
+                    onClick={() => {
+                        updateButtonText();
+                    }}
+                >{ buttonText }</button>
+            </div>
         </div>
+    )
+}
+
+const EditApplication = (props) => {
+    return (
+        <>
+            edit
+        </>
+    )
+}
+
+const DisplayApplication = (props) => {
+    return(
+        <>
+            <TextBox label={ 'Job Title' } text={ props.data['job-title'] }/>
+            <TextBox label={ 'Job Notes' } text={ props.data['job-notes'] } multiline={ true } />
+            <TextBoxLink label={ 'Job Posting' } url={ props.data['job-link'] } />
+            <TextBox label={ 'Company' } text={ props.data['company'] }/>
+            <TextBox label={ 'Company Notes' } text={ props.data['company-notes'] } multiline={ true }/>
+            <TextBox label={ 'Application Status' } text={ props.data['application-status'] }/>
+            <TextBox label={ 'Application Notes' } text={ props.data['application-notes'] } multiline={ true }/>
+        </>
     )
 }
 
