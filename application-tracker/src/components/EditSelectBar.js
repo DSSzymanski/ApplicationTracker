7ab4e1ffa8c6b5d900657799220c3a2a3caf6330
dsszymanski <dan1993us@yahoo.com>
2022-09-13 19:27:52 -0400

    Restructured EditSelectBar by moving options to an object and iterating over object to obtain options elements.
    Added check to make sure that the inputted value is
    one of the options.

--- application-tracker/src/components/EditSelectBar.js
+++ application-tracker/src/components/EditSelectBar.js
@@ -5,20 +5,34 @@ import React from 'react'
  *
  * @param {str} label: string used to display with the label element.
  * @param {str} dataKey: key used in the updateFn callback to update object used in storing data.
- * @param {str} value: string representing current value stored for element. Used to pick the default option upon load. 
+ * @param {str} value: string representing current value stored for element. Used to pick the default option upon load.
  * @param {callback} updateFn: callback function used to pass updated value to be stored in parent element.
  * @returns React component.
  */
 const EditSelectBar = ({ label, dataKey, value, updateFn }) => {
+    const options = {
+        'Applied' : 'Applied',
+        'Called after Applying' : 'Called after Applying',
+        'Interviewing' : 'Interviewing',
+        'Declined after Applying' : 'Declined after Applying',
+        'Declined after Interview' : 'Declined after Interview'
+    }
+
+
     return (
         <div className="input-div">
             <label htmlFor={ label }>{ label }</label>
-            <select name={ label } id={ label } value={ value } onChange={ e => updateFn(dataKey, e.target.value) } >
-                <option value="Applied">Applied</option>
-                <option value="Called after Applying">Called after Applying</option>
-                <option value="Interviewing">Interviewing</option>
-                <option value="Declined after Applying">Declined after Applying</option>
-                <option value="Declined after Interview">Declined after Interview</option>
+            <select
+                name={ label }
+                id={ label }
+                value={ value in options ? value : 'Applied' }
+                onChange={ e => updateFn(dataKey, e.target.value) }
+            >
+                {
+                    Object.keys(options).map((entry, idx) => {
+                        return(<option key={ idx } value={ entry }>{ options[entry] }</option>)
+                    })
+                }
             </select>
         </div>
     )
