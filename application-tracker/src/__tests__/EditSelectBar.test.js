edf237593dfd32df8b0e46d98c76a2c0038a6f2d
dsszymanski <dan1993us@yahoo.com>
2022-09-14 20:53:59 -0400

    Finished tests for select bar.

--- application-tracker/src/__tests__/EditSelectBar.test.js
+++ application-tracker/src/__tests__/EditSelectBar.test.js
@@ -67,6 +67,75 @@ describe('Test suite for the display of EditSelectBar.', () => {
             expect(screen.getByText(option)).toBeInTheDocument();
             unmount();
         }
-    })
+    });
 })
 
+describe('Test suite for functionality of EditSelectBox update function.', () => {
+    test('Test that correct data key is returned by function.', () => {
+        for (let i = 0; i < 1; i++) {
+            //setup
+            let  {test_key, test_label } = setup();
+            let returned_key = '';
+            const test_function = (dataKey, value) => {
+                returned_key = dataKey;
+            }
+            const { unmount } = render(
+                <EditSelectBar
+                    label={ test_label }
+                    dataKey={ test_key }
+                    updateFn={ test_function }
+                />
+            );
+
+            fireEvent.change(screen.getByLabelText(test_label), {target: {value: 2}});
+            expect(returned_key).toBe(test_key);
+            unmount();
+        }
+    });
+
+    test('Test that all select bar options return the correct option when selected.', () => {
+        let { test_label, test_key, select_cases } = setup();
+        for (let i = 0; i < select_cases.length; i++) {
+            let returned_value = '';
+            const test_function = (dataKey, value) => {
+                returned_value = value;
+            }
+            const { unmount } = render(
+                <EditSelectBar
+                    label={ test_label }
+                    dataKey={ test_key }
+                    updateFn={ test_function }
+                />
+            );
+            fireEvent.change(screen.getByLabelText(test_label), {target: {value: select_cases[i]}});
+            expect(returned_value).toBe(select_cases[i]);
+            unmount();
+        }
+    });
+
+    test('Test that both key and value are successfully returned for all select bar options and random keys.', () => {
+        const { select_cases } = setup();
+        for (let j = 0; j < 10; j++) {
+            for (let i = 0; i < select_cases.length; i++) {
+                let { test_label, test_key } = setup();
+                let returned_value = '';
+                let returned_key = '';
+                const test_function = (dataKey, value) => {
+                    returned_key = dataKey;
+                    returned_value = value;
+                }
+                const { unmount } = render(
+                    <EditSelectBar
+                        label={ test_label }
+                        dataKey={ test_key }
+                        updateFn={ test_function }
+                    />
+                );
+                fireEvent.change(screen.getByLabelText(test_label), {target: {value: select_cases[i]}});
+                expect(returned_key).toBe(test_key),
+                expect(returned_value).toBe(select_cases[i]);
+                unmount();
+            }
+        }
+    });
+});
